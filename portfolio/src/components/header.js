7004b81847ecb6f0949cbe92bce2579c33739307
walluri <prakash4455@gmail.com>
2020-01-16 19:59:15 +0530

    PORTFOLIO : Responsive nav bar in sass - animation created

--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Home from './home';
 import Skills from './skills';
@@ -7,19 +7,33 @@ import Experience from './experience';
 import {BrowserRouter, Switch, Route,Link} from 'react-router-dom';
 
 const Header = () => {
+
+    const [togglemenu,setToggleMenu] = useState('off');
+
+    const burger_clickHandler = ()=>{
+        if(togglemenu==='on'){
+            setToggleMenu('off');            
+        }
+        else if (togglemenu==='off'){
+            setToggleMenu('on');
+        }
+        console.log('current state is : '+ togglemenu);
+    } 
+
     return (
         <div>
             <BrowserRouter>
                 <nav className='navbar'>                
-                    <div className="navbar__burger">
-                        <div className="line"></div>
-                        <div className="line"></div>
-                        <div className="line"></div>
+                    <div className={"navbar__burger"} onClick={burger_clickHandler}>
+                        <div className="navbar__burger-line"></div>
+                        <div className="navbar__burger-line"></div>
+                        <div className="navbar__burger-line"></div>
                     </div>
-                    <ul className='navbar__list'>
-                        <li className='navbar__listitems'><Link className='navbar__links' to={'/'}>Home</Link></li>
-                        <li className='navbar__listitems'><Link className='navbar__links' to={'/Skills'}>Skills</Link></li>
-                        <li className='navbar__listitems'><Link className='navbar__links' to={'/Experience'}>Projects</Link></li>
+                    
+                    <ul className={togglemenu==='on' ? "navbar__list animate-list" :"navbar__list"} >
+                        <li className='navbar__listitems'><Link className='navbar__links' to={'/'} onClick={burger_clickHandler}>Home</Link></li>
+                        <li className='navbar__listitems'><Link className='navbar__links' to={'/Skills'} onClick={burger_clickHandler}>Skills</Link></li>
+                        <li className='navbar__listitems'><Link className='navbar__links' to={'/Experience'} onClick={burger_clickHandler}>Projects</Link></li>
                     </ul>
                 </nav>
                 <Switch>
