93a46d73370785a800832dbe109103ac18944c22
satya-valluri <prakash4455@gmail.com>
2020-01-17 18:42:37 +0530

    PORTFOLIO : NavBar deploy

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>Welcome to my place on the internet<hr/>
-            The below technologies are used to develop this website.<hr/>
-            HTML5 - CSS3 + SASS - React - Google Cloud Platform - Dev-ops.
-        </div>
+        <main>
+            <section className="home__section">
+                <div className="home__section-Aboutme">
+                    <h3 className="home__section-heading">welcome to my profile on the internet<br/></h3>
+                    This site is under maintenance and is being developed using CSS3 [flex/grid/sass] - reactJS - Google Cloud Platform - and many other latest technologies.
+                    
+                    <div className="home__section-profilepic">Image</div>
+                    <div className="home__section-description">Description</div>
+                </div>
+            </section>
+        </main>
     )
 }
 
