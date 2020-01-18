e631b301e5b6262f26b71e666c1cc794be866001
walluri <prakash4455@gmail.com>
2020-01-18 20:11:51 +0530

    PORTFOLIO : basic home page ready + production build of react app generated

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -6,10 +6,12 @@ const Home = () => {
             <section className="home__section">
                 <div className="home__section-Aboutme">
                     <h3 className="home__section-heading">welcome to my profile on the internet<br/></h3>
-                    This site is under maintenance and is being developed using CSS3 [flex/grid/sass] - reactJS - Google Cloud Platform - and many other latest technologies.
-                    
-                    <div className="home__section-profilepic">Image</div>
-                    <div className="home__section-description">Description</div>
+                    <br/>This site is under maintenance and is being developed using CSS3 [flex/grid/sass] - reactJS - Google Cloud Platform - and many other latest technologies.
+                    <br/>I am actively looking for opportunities in ReactJS - NodeJS - GraphQL.
+                    <br/>You can visit my LinkedIn profile <a target="_blank" href="www.linkedin.com/in/walluri">here</a>
+                    <br/> You can also see the work and my live projects <a target="_blank" href="https://github.com/walluri">here</a>
+                    {/* <div className="home__section-profilepic">Image</div>
+                    <div className="home__section-description">Description</div> */}
                 </div>
             </section>
         </main>
