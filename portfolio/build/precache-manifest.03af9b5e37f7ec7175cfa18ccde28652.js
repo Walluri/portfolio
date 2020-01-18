1b6417f79ae983cacd097b571a147c7b49210d8c
walluri <prakash4455@gmail.com>
2020-01-18 20:14:26 +0530

    Porfolio : Production build ready React app

---
+++ portfolio/build/precache-manifest.03af9b5e37f7ec7175cfa18ccde28652.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5649dac02fcb2be4ddd161013fe147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87075a794df499fcf31a",
+    "url": "/static/js/2.349f6973.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.349f6973.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07ae7207e47eac95b405",
+    "url": "/static/js/main.0c185bcf.chunk.js"
+  },
+  {
+    "revision": "c0d7c58e510620f2f64c",
+    "url": "/static/js/runtime-main.7b61e083.js"
+  }
+]);
